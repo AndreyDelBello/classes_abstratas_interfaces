970d4bc91c02b3f6aba434e7155e3b9c50c35718
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2021-11-18 22:12:51 +0000

    Abstract

---
+++ Teste.java
@@ -0,0 +1,13 @@
+public class Teste {
+
+    public static void main(String[] args) {
+        
+        ContaEspecializada contaEspecializada=new ContaEspecializada();
+//Contajavac
+        contaEspecializada.setSaldo(25000);
+//ContaEspecializada
+        contaEspecializada.imprimeExtrato();
+
+    }
+    
+}
